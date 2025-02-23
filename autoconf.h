2e5507b0b9e98a5762ddfd299786dfc5f8c84e88
elmagnifico <elmagnificogg@gmail.com>
2025-02-23 13:39:25 +0800

    add lost files

--- autoconf.h
+++ autoconf.h
@@ -1,6 +1,4 @@
-#define CONFIG_PROJECT_NAME "apm32-project"
-#define CONFIG_PROJECT_VERSION "0.0.1"
-#define CONFIG_FreeRTOS 1
-#define CONFIG_LED 1
+#define CONFIG_PROJECT_NAME "Demo-project"
+#define CONFIG_PROJECT_VERSION "0.9.4"
 #define CONFIG_APM32F405 1
-#define CONFIG_Flash 1
+#define CONFIG_FreeRTOS 1
